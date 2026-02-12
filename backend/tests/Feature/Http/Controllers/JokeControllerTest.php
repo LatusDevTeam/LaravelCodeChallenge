1e77e4ba53ab5a80689c495c68bcad88c800b132
Albert Fajarito <albertfajarito@gmail.com>
2026-02-12 14:39:46 +0800

    Add missing tests

--- backend/tests/Feature/Http/Controllers/JokeControllerTest.php
+++ backend/tests/Feature/Http/Controllers/JokeControllerTest.php
@@ -43,4 +43,115 @@ class JokeControllerTest extends TestCase
         // Some extra sanity checks
         $response->assertJsonFragment(['id' => 1, 'setup' => 'setup 1']);
     }
+
+    /**
+     * Tests the jokes endpoint respects the limit filter parameter.
+     */
+    public function test_jokes_endpoint_respects_limit_filter(): void
+    {
+        $fakeJokes = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $fakeJokes[] = [
+                'id' => $i,
+                'type' => 'programming',
+                'setup' => "setup $i",
+                'punchline' => "punchline $i",
+            ];
+        }
+
+        Http::fake([
+            'https://official-joke-api.appspot.com/*' => Http::response($fakeJokes, Response::HTTP_OK),
+        ]);
+
+        // Request with limit=3
+        $response = $this->get('api/jokes?filters[limit]=3');
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(3, 'data');
+        $response->assertJsonStructure([
+            'data' => [
+                ['id', 'type', 'setup', 'punchline'],
+            ],
+        ]);
+    }
+
+    /**
+     * Tests the jokes endpoint returns default count when no limit is specified.
+     */
+    public function test_jokes_endpoint_returns_default_limit_without_filter(): void
+    {
+        $fakeJokes = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $fakeJokes[] = [
+                'id' => $i,
+                'type' => 'programming',
+                'setup' => "setup $i",
+                'punchline' => "punchline $i",
+            ];
+        }
+
+        Http::fake([
+            'https://official-joke-api.appspot.com/*' => Http::response($fakeJokes, Response::HTTP_OK),
+        ]);
+
+        // Request without any filters
+        $response = $this->get('api/jokes');
+
+        $response->assertStatus(Response::HTTP_OK);
+        // Default limit is 10
+        $response->assertJsonCount(10, 'data');
+    }
+
+    /**
+     * Tests the jokes endpoint handles limit=1 (edge case).
+     */
+    public function test_jokes_endpoint_handles_limit_of_one(): void
+    {
+        $fakeJokes = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $fakeJokes[] = [
+                'id' => $i,
+                'type' => 'programming',
+                'setup' => "setup $i",
+                'punchline' => "punchline $i",
+            ];
+        }
+
+        Http::fake([
+            'https://official-joke-api.appspot.com/*' => Http::response($fakeJokes, Response::HTTP_OK),
+        ]);
+
+        $response = $this->get('api/jokes?filters[limit]=1');
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['id' => 1, 'setup' => 'setup 1']);
+    }
+
+    /**
+     * Tests the jokes endpoint handles limit exceeding available jokes.
+     */
+    public function test_jokes_endpoint_handles_limit_exceeding_available(): void
+    {
+        $fakeJokes = [];
+        for ($i = 1; $i <= 5; $i++) {
+            $fakeJokes[] = [
+                'id' => $i,
+                'type' => 'programming',
+                'setup' => "setup $i",
+                'punchline' => "punchline $i",
+            ];
+        }
+
+        Http::fake([
+            'https://official-joke-api.appspot.com/*' => Http::response($fakeJokes, Response::HTTP_OK),
+        ]);
+
+        // Request limit=10 but only 5 jokes available
+        $response = $this->get('api/jokes?filters[limit]=10');
+
+        $response->assertStatus(Response::HTTP_OK);
+        // Should return only 5 (what's available)
+        $response->assertJsonCount(5, 'data');
+    }
 }
